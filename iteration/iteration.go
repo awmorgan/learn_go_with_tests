4a70b05bc371b647060d778bfcb9315f5a18cc2e
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-27 18:05:54 -0500

    add count

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(ch string) string {
+func Repeat(ch string, count int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < count; i++ {
 		repeated += ch	
 	}
 	return repeated
