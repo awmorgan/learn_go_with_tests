92665c024228eee8eb1dcb18539df6b4e8766246
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-27 15:57:05 -0500

    test helper and hello default

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T)  {
-	got := Hello("Bill")
-	want := "Hello, Bill"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Bill")
+		want := "Hello, Bill"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
