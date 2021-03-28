9f30c5685cf4cc5f2f7bd14a68c37c73d931102f
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-28 15:36:04 -0500

    countdown: check order/nr of calls

--- countdown/coutdown_test.go
+++ countdown/coutdown_test.go
@@ -3,30 +3,57 @@ package main
 import (
 	"bytes"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestCountdown(t *testing.T) {
-	b := &bytes.Buffer{}
-	s := &SpySleeper{}
-	Countdown(b, s)
-	got := b.String()
-	want := `3
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		b := &bytes.Buffer{}
+		s := &CountdownOperationsSpy{}
+		Countdown(b, s)
+		got := b.String()
+		want := `3
 2
 1
 Go!
 `
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-	if s.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4, got %d", s)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		s := &CountdownOperationsSpy{}
+		Countdown(s, s)
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !cmp.Equal(want, s.Calls) {
+			t.Errorf("wanted calls %v got %v", want, s.Calls)
+		}
+	})
 }
 
-type SpySleeper struct {
-	Calls int
+type CountdownOperationsSpy struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
 }
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
