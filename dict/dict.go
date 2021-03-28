6b8ed4037769c4148e99f62f415c84cba78e78ba
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-28 12:37:41 -0500

    dict: add more tests

--- dict/dict.go
+++ dict/dict.go
@@ -8,7 +8,10 @@ func main() {
 
 type Dict map[string]string
 
-var ErrNotFound = errors.New("could not find the word")
+var (
+	ErrNotFound = errors.New("could not find the word")
+	ErrWordExists = errors.New("word already exists")
+)
 
 func (d Dict) Search(word string) (string, error) {
 	def, ok := d[word]
@@ -18,6 +21,15 @@ func (d Dict) Search(word string) (string, error) {
 	return def, nil
 }
 
-func (d Dict) Add(word string, def string) {
-	d[word] = def
+func (d Dict) Add(word string, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = def
+		return nil
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
 }
--- dict/dict_test.go
+++ dict/dict_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func assertDef(t testing.TB, dict Dict, word, def string)  {
+func assertDef(t testing.TB, dict Dict, word, def string) {
 	t.Helper()
 	want := def
 	got, err := dict.Search("test")
@@ -15,10 +15,9 @@ func assertDef(t testing.TB, dict Dict, word, def string)  {
 }
 
 func TestSearch(t *testing.T) {
-
 	d := Dict{"test": "this is just a test"}
 	t.Run("known word", func(t *testing.T) {
-		assertDef( t, d, "test", "this is just a test")
+		assertDef(t, d, "test", "this is just a test")
 	})
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := d.Search("unk")
@@ -30,13 +29,34 @@ func TestSearch(t *testing.T) {
 }
 
 func assertStrings(got string, want string, t testing.TB) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given", got, want)
 	}
 }
 
 func TestAdd(t *testing.T) {
-	d := Dict{}
-	d.Add("test", "this is just a test")
-	assertDef( t, d, "test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		d := Dict{}
+		w := "test"
+		def := "this is just a test"
+		err := d.Add(w, def)
+		assertErr(t, err, nil)
+		assertDef(t, d, w, def)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		w := "test"
+		def := "this is just a test"
+		d := Dict{w:def}
+		err := d.Add(w, def)
+		assertErr(t, err, ErrWordExists)
+		assertDef(t, d, w, def)
+	})
+}
+
+func assertErr(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
