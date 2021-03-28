6b21e4d1cac3a63485bf5c5154f87527ab9edfc3
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-28 14:11:40 -0500

    dependency injection

---
+++ di/di.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(w io.Writer, name string) {
+	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+func main() {
+	log.Fatal(http.ListenAndServe(":8000", http.HandlerFunc(MyGreeterHandler)))
+}
