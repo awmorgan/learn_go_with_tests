d5f903d4d3293cad4d949df750546278f88844b7
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-29 17:51:53 -0500

    racer: refactor test

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -8,31 +8,34 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		time.Sleep(5*time.Millisecond)
-		rw.WriteHeader(http.StatusOK)
-	}))
-	fastServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-	}))
+	slowServer := server(5*time.Millisecond)
+	fastServer := server(1*time.Millisecond)
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-
-	want := fastURL
-	got := Racer(slowURL, fastURL)
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-
-	want = fastURL
-	got = Racer(fastURL, slowURL)
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	want := fastServer.URL
+	got := Racer(slowServer.URL, fastServer.URL)
+	assertRacer(got, want, t)
+	
+	want = fastServer.URL
+	got = Racer(fastServer.URL, slowServer.URL)
+	assertRacer(got, want, t)
 
 	slowServer.Close()
 	fastServer.Close()
+}
+
+func assertRacer(got string, want string, t testing.TB) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func server(sleep time.Duration) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		if sleep.Nanoseconds() > 0 {
+			time.Sleep(sleep)
+		}
+		rw.WriteHeader(http.StatusOK)
+	}))
+	return server
 }
