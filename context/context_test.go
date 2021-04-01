95f11acad0ca2eca672316e869035309ea9ce13f
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-04-01 17:30:01 -0500

    test fails

--- context/context_test.go
+++ context/context_test.go
@@ -18,7 +18,7 @@ func (s *SpyStore) Fetch() string {
 	return s.response
 }
 
-func (s *SpyStore) Cancel()  {
+func (s *SpyStore) Cancel() {
 	s.cancelled = true
 }
 
@@ -28,18 +28,36 @@ func TestServer(t *testing.T) {
 		data := "hello, world"
 		store := &SpyStore{response: data}
 		svr := Server(store)
-		
+
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		cancellingCtx, cancel := context.WithCancel(request.Context())
-		time.AfterFunc(5 *time.Millisecond, cancel)
+		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(cancellingCtx)
-		
+
 		response := httptest.NewRecorder()
-		
+
 		svr.ServeHTTP(response, request)
 
 		if !store.cancelled {
 			t.Errorf("store was not told to cancel")
+		}
+	})
+	t.Run("returns data from store", func(t *testing.T) {
+
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+		if store.cancelled {
+			t.Errorf("it should not have cancelled the store")
 		}
 	})
 }
